021165dc533cbdfdabcbf7cb70a9ef2d311dab56
Ates Goral <ates.g@myplanet.com>
2015-10-27 10:22:24 -0400

    Externalized rando item picker

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var pf = require('quick-primefactors'),
+var pick = require('./pick'),
+    pf = require('quick-primefactors'),
     levels = require('hrm-level-data');
 
 var tilesForLevel = {};
@@ -6,72 +7,6 @@ var tilesForLevel = {};
 levels.forEach(function (level) {
     tilesForLevel[level.number] = level.floor && level.floor.tiles;
 });
-
-function randomNumberBetween(min, max) {
-    var number;
-
-    do {
-        number = Math.floor(Math.random() * (max - min + 1)) + min;
-    } while (number === 0);
-
-    return number;
-}
-
-function Slots(count) {
-    this._slots = new Array(count);
-}
-
-Slots.prototype.toArray = function () {
-    return this._slots.slice(0);
-};
-
-Slots.prototype.numbersBetween = function (min, max) {
-    this._slots = this._slots.map(function (item) {
-        return this._or && Math.random() > 0.5
-            ? item
-            : randomNumberBetween(min, max);
-    }, this);
-
-    return this;
-};
-
-Slots.prototype.letters = function () {
-    var a = 'A'.charCodeAt(0),
-        z = 'Z'.charCodeAt(0);
-
-    this._slots = this._slots.map(function (item) {
-        return this._or && Math.random() > 0.5
-            ? item
-            : String.fromCharCode(randomNumberBetween(a, z));
-    }, this);
-
-    return this;
-};
-
-Slots.prototype.zero = function () {
-    this._slots = this._slots.map(function (item) {
-        return this._or && Math.random() > 0.5
-            ? item
-            : 0;
-    }, this);
-
-    return this;
-};
-
-Slots.prototype.or = function () {
-    this._or = true;
-    return this;
-};
-
-var pick = {
-    exactly: function (count) {
-        return new Slots(count);
-    },
-    between: function (min, max) {
-        return new Slots(randomNumberBetween(min, max));
-    }
-};
-
 
 var generators = {};
 
